960e210ba4e9fd62eb4814160859919af9e1df46
Ahsan Habib Tamim <ahsanhabibtamim.new@gmail.com>
2026-07-14 00:43:39 +0600

    Update routineTypes.ts

--- src/routineTypes.ts
+++ src/routineTypes.ts
@@ -1,1 +1,21 @@
+export interface RoutineItem {
+  id: string;
+  department: string;
+  batch: string;
+  section: string;
+  subsection: string; // e.g. "A", "B", or ""
+  fullSectionString: string; // e.g. "1B", "2A", "3", "Old", "SS"
+  day: string; // e.g. "SATURDAY", "SUNDAY", etc.
+  timeSlot: string; // e.g. "09:10 AM-10:25 AM"
+  courseName: string;
+  courseCode: string;
+  roomNo: string;
+  facultyInitial: string;
+}
 
+export interface FilterOptions {
+  department: string;
+  batch: string;
+  section: string;
+  subsection: string;
+}
