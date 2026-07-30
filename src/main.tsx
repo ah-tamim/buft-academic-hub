00431a785eadf34f565ec1afc0008e4d682699e2
Ahsan Habib Tamim <ahsanhabibtamim.new@gmail.com>
2026-07-30 21:07:55 +0600

    Wrap App component with AppConfigProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
+import { AppConfigProvider } from './context/AppConfigContext.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppConfigProvider>
+      <App />
+    </AppConfigProvider>
   </StrictMode>,
 );
